9687cd3084c0c78e6b3a0aa60924fdd81b58b419
dacchen <15526880747@163.com>
2023-12-19 12:09:29 +0800

    dacchen,feat:实现和使用分库分表

---
+++ lottery-infrastructure/src/main/java/top/dacchen/lottery/infrastructure/dao/IUserStrategyExportDao.java
@@ -0,0 +1,35 @@
+package top.dacchen.lottery.infrastructure.dao;
+
+
+import org.apache.ibatis.annotations.Mapper;
+import top.dacchen.lottery.infrastructure.po.UserStrategyExport;
+import top.dacchen.middleware.db.router.annotation.DBRouter;
+import top.dacchen.middleware.db.router.annotation.DBRouterStrategy;
+
+/**
+ * @description: 用户策略计算结果表DAO
+ * @author: 小傅哥，微信：fustack
+ * @date: 2021/9/30
+ * @github: https://github.com/fuzhengwei
+ * @Copyright: 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ */
+@Mapper
+@DBRouterStrategy(splitTable = true)
+public interface IUserStrategyExportDao {
+
+    /**
+     * 新增数据
+     * @param userStrategyExport 用户策略
+     */
+    @DBRouter(key = "uId")
+    void insert(UserStrategyExport userStrategyExport);
+
+    /**
+     * 查询数据
+     * @param uId 用户ID
+     * @return 用户策略
+     */
+    @DBRouter
+    UserStrategyExport queryUserStrategyExportByUId(String uId);
+
+}
